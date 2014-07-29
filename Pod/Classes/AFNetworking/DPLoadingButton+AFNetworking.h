411ff703e3a8155029ae172b5fb6e30af8f14ee4
Petroianu Daniel <petroianudaniel@gmail.com>
2014-07-29 21:19:46 +0300

    added AFNetworking helper

---
+++ Pod/Classes/AFNetworking/DPLoadingButton+AFNetworking.h
@@ -0,0 +1,50 @@
+//
+//  DPLoadingButton+AFNetworking.h
+//  Pods
+//
+//  Created by Petroianu Daniel on 7/25/14.
+//
+//
+
+#import "DPLoadingButton.h"
+
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+
+@class AFURLConnectionOperation;
+typedef AFURLConnectionOperation *(^DPLoadingButtonRequestOperationAction)(DPLoadingButton *button);
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
+typedef NSURLSessionTask *(^DPLoadingButtonURLSessionTaskAction)(DPLoadingButton *button);
+#endif
+
+/**
+ [COPY OF AFNetworking/UIActivityIndicatorView+AFNetworking.h]
+ Category over AFNetworking that automatically starts and stops animation depending on the loading state of a request operation or session task
+ */
+@interface DPLoadingButton (AFNetworking)
+
+
+
+
+/**
+ Register a block that will return and AFURLConnectionOperation used to animate the UIActivityIndicatorView from the button.
+ 
+ @param block A block of code that will be executed when the button is pressed
+ */
+- (void)startRequestOperationOnButtonTap:(DPLoadingButtonRequestOperationAction)block;
+
+
+
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
+/**
+ Register a block that will return and NSURLSessionTask used to animate the UIActivityIndicatorView from the button.
+ 
+ @param block A block of code that will be executed when the button is pressed
+ */
+- (void)startTaskOnButtonTap:(DPLoadingButtonURLSessionTaskAction)block; NS_AVAILABLE_IOS(7.0);
+#endif
+
+@end
+
+#endif
---
+++ Pod/Classes/AFNetworking/DPLoadingButton+AFNetworking.m
@@ -0,0 +1,105 @@
+//
+//  DPLoadingButton+AFNetworking.m
+//  Pods
+//
+//  Created by Petroianu Daniel on 7/25/14.
+//
+//
+
+#import "DPLoadingButton+AFNetworking.h"
+#import <objc/runtime.h>
+
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+
+#import <AFNetworking/AFURLConnectionOperation.h>
+static char RequestOperationButtonTapBlock;
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
+#import <AFNetworking/AFURLSessionManager.h>
+static char NSURLSessionTaskButtonTapBlock;
+#endif
+
+
+@implementation DPLoadingButton (AFNetworking)
+
+#pragma mark - AFURLConnectionOperation
+
+- (void)startRequestOperationOnButtonTap:(DPLoadingButtonRequestOperationAction)block {
+    [self willChangeValueForKey:@"RequestOperationButtonTapBlock"];
+    objc_setAssociatedObject(self, &RequestOperationButtonTapBlock, block, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    [self didChangeValueForKey:@"RequestOperationButtonTapBlock"];
+    
+    [self addTarget:self action:@selector(dp_requestOperation_onButtonTap:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)dp_requestOperation_onButtonTap:(id)sender {
+    DPLoadingButtonRequestOperationAction block = objc_getAssociatedObject(self, &RequestOperationButtonTapBlock);
+    if(block) {
+        AFURLConnectionOperation *requestOperation = block(self);
+        [self startAnimatingWithStateOfOperation:requestOperation];
+    }
+}
+
+- (void)startAnimatingWithStateOfOperation:(AFURLConnectionOperation *)operation {
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self name:AFNetworkingOperationDidStartNotification object:nil];
+    [notificationCenter removeObserver:self name:AFNetworkingOperationDidFinishNotification object:nil];
+    
+    if (operation && ![operation isFinished]) {
+        if ([operation isExecuting]) {
+            [self startAnimating];
+        } else {
+            [self stopAnimating];
+        }
+        
+        [notificationCenter addObserver:self selector:@selector(startAnimating) name:AFNetworkingOperationDidStartNotification object:operation];
+        [notificationCenter addObserver:self selector:@selector(stopAnimating) name:AFNetworkingOperationDidFinishNotification object:operation];
+    }
+}
+
+
+#pragma mark - NSURLSessionTask
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000
+
+- (void)startTaskOnButtonTap:(DPLoadingButtonURLSessionTaskAction)block {
+    [self willChangeValueForKey:@"NSURLSessionTaskButtonTapBlock"];
+    objc_setAssociatedObject(self, &NSURLSessionTaskButtonTapBlock, block, OBJC_ASSOCIATION_COPY_NONATOMIC);
+    [self didChangeValueForKey:@"NSURLSessionTaskButtonTapBlock"];
+    
+    [self addTarget:self action:@selector(dp_sessionTask_onButtonTap:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)dp_sessionTask_onButtonTap:(id)sender {
+    DPLoadingButtonURLSessionTaskAction block = objc_getAssociatedObject(self, &NSURLSessionTaskButtonTapBlock);
+    if(block) {
+        NSURLSessionTask *task = block(self);
+        [self startAnimatingWithStateOfTask:task];
+    }
+}
+
+- (void)startAnimatingWithStateOfTask:(NSURLSessionTask *)task {
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self name:AFNetworkingTaskDidResumeNotification object:nil];
+    [notificationCenter removeObserver:self name:AFNetworkingTaskDidSuspendNotification object:nil];
+    [notificationCenter removeObserver:self name:AFNetworkingTaskDidCompleteNotification object:nil];
+    
+    if (task && task.state != NSURLSessionTaskStateCompleted) {
+        if (task.state == NSURLSessionTaskStateRunning) {
+            [self startAnimating];
+        } else {
+            [self stopAnimating];
+        }
+        
+        [notificationCenter addObserver:self selector:@selector(startAnimating) name:AFNetworkingTaskDidResumeNotification object:task];
+        [notificationCenter addObserver:self selector:@selector(stopAnimating) name:AFNetworkingTaskDidCompleteNotification object:task];
+        [notificationCenter addObserver:self selector:@selector(stopAnimating) name:AFNetworkingTaskDidSuspendNotification object:task];
+    }
+}
+
+#endif
+
+
+@end
+
+#endif
