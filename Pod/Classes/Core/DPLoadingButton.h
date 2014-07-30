419d28fc45780c0bb000afce18fc565fb311f999
Petroianu Daniel <petroianudaniel@gmail.com>
2014-07-30 12:34:28 +0300

    updated documentation

--- Pod/Classes/Core/DPLoadingButton.h
+++ Pod/Classes/Core/DPLoadingButton.h
@@ -15,15 +15,17 @@ typedef void (^DPLoadingButtonCompletionHandler)(DPLoadingButton *button);
 
 
 /**
- A 'button like' control that displayes a UIActivityIndicatorView when the button is tapped.
+ A 'button like' control that displayes a UIActivityIndicatorView when the button fires a control event.
  */
 @interface DPLoadingButton : UIControl
 
 /**
  The UIActivityIndicatorView that will be used to display the loading animation.
  
- @discussion
- Although this property is read-only, its own properties are read/write. Use these properties to configure the style of the loading view. For example:
+ @note   Although this property is read-only, its own properties are read/write. Use these properties to configure the style of the loading view.
+ 
+ @example
+ 
     DPLoadingButton *button                                 = [[DPLoadingButton alloc] initWithTitle:@"some title"];
     button.activityIndicatorView.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
     button.activityIndicatorView.color                      = [UIColor blackColor];
@@ -33,12 +35,14 @@ typedef void (^DPLoadingButtonCompletionHandler)(DPLoadingButton *button);
 /**
  A view that displays the value the title used when the button was initialized.
  
- @discussion
- Although this property is read-only, its own properties are read/write. Use these properties to configure the text of the button. For example:
+ @note   Although this property is read-only, its own properties are read/write. Use these properties to configure the text of the button.
+         If - (instancetype)initWithTitle:(NSString *)title wasn't used this will be nil.
+ 
+ @example
+ 
     DPLoadingButton *button           = [[DPLoadingButton alloc] initWithTitle:@"some title"];
     button.titleLabel.font            = [UIFont systemFontOfSize: 12];
     button.titleLabel.lineBreakMode   = UILineBreakModeTailTruncation;
- If - (instancetype)initWithTitle:(NSString *)title wasn't used this will be nil.
  */
 @property(nonatomic, strong, readonly) UILabel *titleLable;
 
@@ -46,62 +50,71 @@ typedef void (^DPLoadingButtonCompletionHandler)(DPLoadingButton *button);
 /**
  Initializes a new item using the specified image.
  
- @param image The image to display in the button.
+ @return [DPLoadingButton] Returns an initialized DPLoadingButton object or nil if the object couldn't be created.
  
- @return An initialized DPLoadingButton object or nil if the object couldn't be created.
+ @param  [UIImage] image The image to display in the button.
  
- @discussion This method adjusts the frame of the receiver to match the size of the specified image.
+ @note   This method adjusts the frame of the button to match the size of the specified image.
  */
 - (instancetype)initWithImage:(UIImage *)image;
 
 /**
  Initializes a new item using the specified title.
+
+ @return [DPLoadingButton] Returns an initialized DPLoadingButton object or nil if the object couldn't be created.
  
- @param title The text to display in the button.
+ @param  [NSString] title The text to display in the button.
  
- @return An initialized DPLoadingButton object or nil if the object couldn't be created.
- 
- @discussion This method adjusts the frame of the receiver to match the size of the specified title.
+ @note   This method adjusts the frame of the button to match the size of the specified title.
  */
 - (instancetype)initWithTitle:(NSString *)title;
 
 /**
  Initializes a new item using the specified custom view.
  
- @param view A custom view representing the item.
+ @return [DPLoadingButton] Returns an initialized DPLoadingButton object or nil if the object couldn't be created.
  
- @return An initialized DPLoadingButton object or nil if the object couldn't be created.
+ @param  [UIView] view A custom view representing the item.
  
- @discussion This method adjusts the frame of the receiver to match the size of the specified view. It also disables user interactions for the custom view by default, so that the tap action on the button could be recognised.
+ @note   This method adjusts the frame of the button to match the size of the specified view. 
+         It also disables user interactions for the custom view by default, so that the button control events could be recognised.
  */
 - (instancetype)initWithCustomView:(UIView *)view;
 
 /**
  Register action blocks that will be executed when the specified controlEvents happen.
  
- @param action        A block that will be executed on a background thread on the button 'controlEvents'.
- @param controlEvents A bitmask specifying the control events for which the action block is executed. @see UIControlEvents.
+ @param  [DPLoadingButtonAction] action        A block that will be executed on a background thread on the button 'controlEvents'.
+ @param  [UIControlEvents] controlEvents A bitmask specifying the control events for which the action block is executed. @see UIControlEvents.
+ 
+ @note   The 'action' block execution will be done on a background thread.
  */
 - (void)addAction:(DPLoadingButtonAction)action forControlEvents:(UIControlEvents)controlEvents;
 
 /**
  Register action blocks that will be executed when the specified controlEvents happen.
  
- @param action            A block that will be executed on a background thread on the button 'controlEvents'.
- @param completionHandler A block that will be executed after the action block execution
- @param controlEvents     A bitmask specifying the control events for which the action block is executed. @see UIControlEvents.
+ @param  [DPLoadingButtonAction] action        A block that will be executed on a background thread on the button 'controlEvents'.
+ @param  [DPLoadingButtonCompletionHandler] completionHandler A block that will be executed after the action block execution
+ @param  [UIControlEvents] controlEvents A bitmask specifying the control events for which the action block is executed. @see UIControlEvents.
+ 
+ @note   The 'action' block execution will be done on a background thread and the 'completionHandler' block will be execution will be done on the main thread.
  */
 - (void)addAction:(DPLoadingButtonAction)action withCompletion:(DPLoadingButtonCompletionHandler)completionHandler forControlEvents:(UIControlEvents)controlEvents;
 
 /**
  Calls the startAnimating methond on the activityIndicatorView.
- @discussion This will make sure that the animation is done on the main thread. It will automatically be called before onButtonTap is called.
+ 
+ @note   This will make sure that the animation is done on the main thread.
+         It will automatically be called before the 'action' block is executed, but in case you need to show it as animating for some other reason you can call it.
  */
 - (void)startAnimating;
 
 /**
  Calls the stopAnimating methond on the activityIndicatorView.
- @discussion This will make sure that the animation is done on the main thread. It will automatically be called after onButtonTap is called.
+ 
+ @note   This will make sure that the animation is done on the main thread.
+         It will automatically be called before the 'action' block is executed, but in case you need to show it as animating for some other reason you can call it.
  */
 - (void)stopAnimating;
 
@@ -116,7 +129,7 @@ typedef void (^DPLoadingButtonCompletionHandler)(DPLoadingButton *button);
 /**
  Helper method that will return an UIBarButtonItem.
  
- @return A UIBarButtonItem with this DPLoadingButton as it's view.
+ @return [UIBarButtonItem] A UIBarButtonItem with this DPLoadingButton as it's view.
  */
 - (UIBarButtonItem *)toBarButtonItem;
 
